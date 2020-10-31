f1d7d52d2071ec6722b939ef32144a9f72f04f06
Blania <piekarz.blanka@gmail.com>
2020-10-31 13:03:02 +0100

    Sort population, add algorithm usage

--- src/GeneticAlgorithm.py
+++ src/GeneticAlgorithm.py
@@ -27,5 +27,31 @@ def generate_population(size, x_boundaries, y_boundaries):
 def apply_function(individual):
     x = individual["x"]
     y = individual["y"]
+    # TODO: gets some error, fix the function
     return sqrt(sin(3*x*pi)) + sqrt(x-1)*(1+sqrt(sin(3*y*pi))) + sqrt(y-1)*(1+sqrt(2*y*pi))
 
+def sort_population_by_fitness(population):
+    return sorted(population, key=apply_function)
+
+# ----------------------------------
+
+generations = 5
+
+population = generate_population(size=10, x_boundaries=(-10, 10), y_boundaries=(-4, 4))
+
+i = 1
+while True:
+    print(f"GENERATION {i}")
+
+    for individual in population:
+        print(individual)
+
+    if i == generations:
+        break
+
+    i += 1
+
+
+best_individual = sort_population_by_fitness(population)[-1]
+print("\nFINAL RESULT")
+print(best_individual, apply_function(best_individual))
